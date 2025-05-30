a00110c6bde22d46c8f45204b0c236cae800b1ab
Tashinga Ryan Manunure <116067397+garandnounours@users.noreply.github.com>
2025-05-30 12:41:40 +0200

    render upload settings

--- src/database/migrations/add_password_to_landing_user.sql
+++ src/database/migrations/add_password_to_landing_user.sql
@@ -1,5 +1,5 @@
 -- Add password column to landing_page_user table
 ALTER TABLE "landing_page_user" ADD COLUMN "password" VARCHAR(255);
-
+ 
 -- Make password required for future records
 ALTER TABLE "landing_page_user" ALTER COLUMN "password" SET NOT NULL; 
